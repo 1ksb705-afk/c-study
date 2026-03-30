4281ab46ac2ef6ce72ea746fefb8a50bfeebd259
soonbaek <1ksb705@gmail.com>
2026-03-30 14:44:23 +0900

    c언어 실습

---
+++ array/char-array-1.c
@@ -0,0 +1,31 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main(void)
+//{
+//	char str[30] = "C language"; //문자배열 30자
+//	int i = 0;
+//
+//	while (str[i] != '\0') //만족할때 반복 // != 다르다
+//							//문자열 내용 끝나는시점에 반드시 null이 들어감
+//		i++;
+//
+//	printf("문자열\"%s\"의 길이는 %d입니다.\n", str, i);
+//	return 0;
+//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int a;
+	char menu[5][10] = { //5행 10열(0~4행 0~9열)
+	"init", //1행씩 초기값 설정
+	"open",
+	"close",
+	"read",
+	"write"
+	};
+
+	for (a = 0; a < 5; a++) //0~4
+		printf("%d 번째 메뉴: %s \n", a, menu[a]);
+	return 0;
+}
---
+++ array/char-array-2.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int a;
+	char fruits[3][20];
+
+	for (a = 0; a < 3; a++) //0~2
+	{
+		printf("과일 이름을 입력하세요: ");
+		scanf("%s", fruits[a]);
+		printf("%d 번째 과일은 : %s \n", a, fruits[a]);
+	}
+
+	return 0;
+}
