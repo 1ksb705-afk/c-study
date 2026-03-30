d5f6d760ec6a3ca9677cbf5d89277e16e4cc12f4
soonbaek <1ksb705@gmail.com>
2026-03-30 15:21:48 +0900

    문자 배열 실습

---
+++ array/2array-2.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int a;
+	char menu[3][10] = { //5행 10열(0~4행 0~9열)
+	"init", //1행씩 초기값 설정
+	"open",
+	"close",
+	"read",
+	"write"
+	};
+
+	for (a = 0; a < 3; a++) //0~4
+		printf("%d 번째 메뉴: %s \n", a, menu[a]);
+	return 0;
+}
