19eef191dc407e65765fe21725d4eb10b3ff2b57
soonbaek <1ksb705@gmail.com>
2026-04-02 12:48:46 +0900

    포인터

---
+++ pointer/pointer-2.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int i = 3000;
+	int* p = NULL; // 포인터변수 선언
+
+	p = &i; //i변수의 주소를  p포인터 변수에 줌
+
+	printf("&i = %p\n\n", &i);
+	//%p : 주소를 출력하는 서식 문자
+	printf("&i = %p\n\n", &i);
+
+	printf("i = %d\n", i);
+	printf("*p = %d\n", *p);
+	//주소에 저장된 내용에 접근하는 포인터
+	i = 4000;
+	printf("i = %d\n", i);
+	*p = 5000;
+	printf("*p = %d\n", *p);
+	//---------------------------------------------------------
+	int x = 10, y = 20;
+	int* pi; //포인터 함수
+
+	pi = &x; //x의 주소를 포인터 변수 pi에 준다
+	printf("pi = %p\n", pi); // pi가 가르키는 x의 주소를 x에 출력한다. //10
+	printf("*pi = %p\n", *pi);
+
+	pi = &y;
+	printf("pi = %p\n", pi); //y의 주소를 포인터 변수 pi에 준다
+	printf("*pi = %p\n", *pi); //pi가 가르키는 y의 주소를 y에 출력한다. //20
+
+	return 0;
+}
--- self-learning/function.c
+++ self-learning/function.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 int digit(int n) { //digit라는 함수를 선언
-	if (n < 10) return n; //만약 n이 10보다 작다면 10을 돌려보낸다.
+	if (n < 10) return n; //만약 n이 10보다 작다면 10을 돌려보낸다. 
 	return (n % 10) + digit(n / 10); //n이 10 이상일 때 맨 끝자리와 나머지 앞 부분을 합한다.
+	//(235%10) + digit(235/10)
+	//5+ digit(23)
+	//(23%10) + digit(23/10)
+	//   3+digit(2)
+	
 }
+
+
+
+
 int main() { //제일 먼저 시작되는 부분
 	printf("%d", digit(235)); //235를 시행하여 결과 %d를 출력(10)
 }
